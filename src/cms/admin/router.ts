b2378cff380a4dfd042222defc7267af5d23bbdb
Lane <>
2023-04-30 08:06:04 -0700

    hono and d1 inti

--- src/cms/admin/router.ts
+++ src/cms/admin/router.ts
@@ -1,32 +1,34 @@
-import { Router } from "itty-router";
-export const adminRouter = Router();
+import { Hono } from "hono";
+import { html } from "hono/html";
 
-const html = theme => `
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Todos</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet"></link>
-  </head>
-  <body class="bg-blue-100">
-    <div class="w-full h-full flex content-center justify-center mt-8">
-      <div class="bg-white shadow-md rounded px-8 pt-6 py-8 mb-4">
-        <h1 class="block text-grey-800 text-md font-bold mb-2">Todos</h1>
-        <div class="flex">
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-800 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" placeholder="A new todo"></input>
-          <button class="bg-blue-500 hover:bg-blue-800 text-white font-bold ml-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline" id="create" type="submit">Create</button>
-        </div>
-        <div class="mt-4" id="todos"></div>
-      </div>
-    </div>
-  </body>
-</html>
-`
+const app = new Hono();
 
-adminRouter.get("/admin", () => {
-    return new Response(html);
-  });
+// const html = theme => `
+// <!DOCTYPE html>
+// <html>
+//   <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width,initial-scale=1">
+//     <title>Todos</title>
+//     <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet"></link>
+//   </head>
+//   <body class="bg-blue-100">
+//     <div class="w-full h-full flex content-center justify-center mt-8">
+//       <div class="bg-white shadow-md rounded px-8 pt-6 py-8 mb-4">
+//         <h1 class="block text-grey-800 text-md font-bold mb-2">Todos</h1>
+//         <div class="flex">
+//           <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-800 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" placeholder="A new todo"></input>
+//           <button class="bg-blue-500 hover:bg-blue-800 text-white font-bold ml-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline" id="create" type="submit">Create</button>
+//         </div>
+//         <div class="mt-4" id="todos"></div>
+//       </div>
+//     </div>
+//   </body>
+// </html>
+// `
+
+app.get("/", (c) => {
+  return c.html("hello admin");
+});
 
 // export router;
