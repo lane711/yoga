969937e62e868615cafa9be2491bf8f52cbdec20
Lane <>
2023-05-01 22:10:31 -0700

    add jest

--- src/cms/data/data.test.ts
+++ src/cms/data/data.test.ts
@@ -1,6 +1,16 @@
-const sum = require('./data');
-import {test} from 'jest'
+import { add, getKey } from "./data";
+describe("test add function", () => {
+  it("should return 15 for add(10,5)", () => {
+    expect(add(10, 5)).toBe(15);
+  });
+  it("should return 5 for add(2,3)", () => {
+    expect(add(2, 3)).toBe(5);
+  });
+});
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+it("should generate a key", () => {
+  const key = getKey("site", "module");
+  expect(key.startsWith("site::module")).toBe(true);
+  expect(key.length).toBe(40);
+
 });
