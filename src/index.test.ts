10a2ed2ce6ac3148e9db72ec1a3c9f03f25f0c01
Lane <>
2023-05-07 19:54:16 -0700

    jest post

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,30 @@
 import app from './index'
 
+const baseUrl = 'http://127.0.0.1:3077';
+
 describe('API Tests', () => {
     test('GET forms', async () => {
-      const res = await app.request('http://127.0.0.1:3077/api/test');
+      const res = await app.request(`${baseUrl}/api/test`);
       expect(res.status).toBe(200)
       expect(await res.text()).toBe('Many posts')
     })
+
+    test('GET form components', async () => {
+        const res = await app.request(`${baseUrl}/api/form-components`);
+        expect(res.status).toBe(200)
+        const formComponents = await res.text();
+        expect(Array.isArray(JSON.parse(formComponents))).toBe(true)
+      })
+
+      test('POST /posts', async () => {
+        const req = new Request(`${baseUrl}/api/form-components`, {
+          method: 'POST',
+        })
+        const res = await app.request(req)
+        expect(res.status).toBe(201)
+        expect(res.headers.get('X-Custom')).toBe('Thank you')
+        expect(await res.json()).toEqual({
+          message: 'Created',
+        })
+      })
   })
